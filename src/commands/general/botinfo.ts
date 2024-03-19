27865142729db68f725a70c366970e3df2420b91
StainlessDev <57878874+StainlessDev@users.noreply.github.com>
2024-03-18 23:37:33 -0400

    Delete botinfo.ts

--- src/commands/general/botinfo.ts
+++
@@ -1,9 +0,0 @@
-import{ Command }from "@structures/command"
-
-export default new Command(
-    name: "ping",
-    description: "Ping pong!",
-    run: async(client, interaction, args) => {
-        return await interaction.reply("Pong!");
-    }
-)
